fix hand cluster normalization, add plotting normalized hand cluster, add som implementation

radar_data_labeler.py
@@ -15,13 +15,13 @@ from scipy.spatial import distance
 # radarData_path = 'F:/onNotOn_data/072819_zl_onNotOn/f_data-2019-07-28_22-11-01.258054_zl_onNotOn_rnn/f_data.p'
 # videoData_path = 'F:/onNotOn_data/072819_zl_onNotOn/v_data-2019-07-28_22-10-32.249041_zl_onNotOn_rnn/cam1'
 # mergedImg_path = 'F:/figures/new'
-raw_path = 'F:/onNotOn_raw/zl_onNoton_raw.p'
+# raw_path = 'F:/onNotOn_raw/zl_onNoton_raw.p'
 
 # ag path
 # radarData_path = 'F:/onNotOn_data/072819_ag_onNotOn/f_data-2019-07-28_21-44-17.102820_ag_onNotOn_rnn/f_data.p'
 # videoData_path = 'F:/onNotOn_data/072819_ag_onNotOn/v_data-2019-07-28_21-44-08.514321_ag_onNotOn_rnn/cam1'
 # mergedImg_path = 'F:/figures/ag_onNotOn_x03y03z03_clustered_esp02ms4'
-raw_path = 'F:/onNotOn_raw/ag_onNoton_raw.p'
+# raw_path = 'F:/onNotOn_raw/ag_onNoton_raw.p'
 
 # zy path
 radarData_path = 'F:/onNotOn_data/072919_zy_onNotOn/f_data.p'