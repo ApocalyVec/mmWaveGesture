4da6b52aae5b6f6dd60db2f94663861b3c63d4a7
hanfei <hfsun98@gmail.com>
2019-08-04 22:48:49 -0400

    add testing script

---
+++ test.py
@@ -0,0 +1,45 @@
+from utils import label
+
+# df, not_found = label('/Users/hanfei/label_2/label_2', '/Users/hanfei/onNotOn_data.csv')
+#
+# print(len(not_found))
+# not_found = sorted(not_found)
+# with open('missing_timestamp.txt', 'w') as writer:
+#     for ts in not_found:
+#         writer.write(str(ts) + '\n')
+# writer.close()
+# print('finished')
+import os
+lst = []
+# path = '/Users/hanfei/onNoton_figures'
+path = '/Users/hanfei/figures'
+data_file = '/Users/hanfei/onNotOn_data.csv'
+for folder in os.listdir(path):
+    if not (folder == '.DS_Store' or folder == 'utils'):
+        print(folder)
+        sub_list = os.listdir(os.path.join(path, folder))
+        if '.DS_Store' in sub_list: sub_list.remove('.DS_Store')
+        sub_list = list(map(lambda x: x.strip('.jpg'), sub_list))
+        lst.extend(sub_list)
+
+assert isinstance(lst[0], str)
+
+import pandas as pd
+data = pd.read_csv(data_file)
+
+timestamp_set = set()
+for timestamp in range(len(data)):
+    timestamp_set.add(data.loc[timestamp].iat[1])
+timestamp_set = list(map(lambda x: str(timestamp.as_integer_ratio()[0]) + '_' + str(timestamp.as_integer_ratio()[1], timestamp_set)))
+
+assert isinstance(timestamp_set[0], str)
+
+print('# of timestamps from images: ' + str(len(lst)))
+print('# of timestamps from csv: ' + str(len(timestamp_set)))
+
+result = []
+for item in lst:
+    if item not in timestamp_set: result.append(item)
+
+print('# of missing timestamps: ' + str(len(result)))
+print('example: ' + result[0])
