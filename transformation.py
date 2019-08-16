52d848c4ea113c65ef9e07eb9094a253eacab2ff
hanfei <hfsun98@gmail.com>
2019-08-15 23:10:17 -0400

    implement several transformation algorithms

--- transformation.py
+++ transformation.py
@@ -1,3 +1,22 @@
+import numpy as np
+
+def transform(m, trans_m):
+    """
+    NOTE: this function is called by other transformation functions, do not call this function directly
+    :param m: the n * 3 matrix for which to apply translate
+    :param trans_m: the 4 * 4 transformation matrix depending on the transformation type
+    :return: the transformed matrix m
+    """
+
+    result = np.zeros((m.shape[0], m.shape[1]))
+
+    for row in range(m.shape[0]):
+        # a single point [x, y, z, 1]
+        tmp = np.append(m[row], 1.).reshape(4, 1)
+        # apply translation to a single point
+        result[row] = np.delete(np.matmul(trans_m, tmp).reshape(-1), 3)
+    return result
+
 def translate(m, x, y, z):
     """
 
@@ -6,6 +25,13 @@ def translate(m, x, y, z):
     :param y:
     :param z:
     """
+    translation_matrix = np.array([
+        [1., 0., 0., x],
+        [0., 1., 0., y],
+        [0., 0., 1., z],
+        [0., 0., 0., 1.]
+    ])
+    return transform(m, translation_matrix)
 
 
 def scale(m, x, y, z):
@@ -16,6 +42,21 @@ def scale(m, x, y, z):
     :param y:
     :param z:
     """
+    scaling_matrix = np.array([
+        [x, 0., 0., 0.],
+        [0., y, 0., 0.],
+        [0., 0., z, 0.],
+        [0., 0., 0., 1.]
+    ])
+    center = np.mean(m, axis=0)
+    offset_x = center[0]
+    offset_y = center[1]
+    offset_z = center[2]
+
+    tmp = translate(m, -offset_x, -offset_y, -offset_z)
+    tmp = transform(tmp, scaling_matrix)
+    return translate(tmp, offset_x, offset_y, offset_y)
+
 
 
 def rotateX(m, theta):
@@ -26,6 +67,14 @@ def rotateX(m, theta):
     :param y:
     :param z:
     """
+    theta_radian = np.radians(theta)
+    rotation_matrix = np.array([
+        [1., 0., 0., 0.],
+        [0., np.cos(theta_radian), -np.sin(theta_radian), 0.],
+        [0., np.sin(theta_radian), np.cos(theta_radian), 0.],
+        [0., 0., 0., 1.]
+    ])
+    return transform(m, rotation_matrix)
 
 def rotateY(m, theta):
     """
@@ -35,6 +84,14 @@ def rotateY(m, theta):
     :param y:
     :param z:
     """
+    theta_radian = np.radians(theta)
+    rotation_matrix = np.array([
+        [np.cos(theta_radian), 0., np.sin(theta_radian), 0.],
+        [0., 1., 0., 0.],
+        [-np.sin(theta_radian), 0., np.cos(theta_radian), 0.],
+        [0., 0., 0., 1.]
+    ])
+    return transform(m, rotation_matrix)
 
 def rotateZ(m, theta):
     """
@@ -44,3 +101,37 @@ def rotateZ(m, theta):
     :param y:
     :param z:
     """
+    theta_radian = np.radians(theta)
+    rotation_matrix = np.array([
+        [np.cos(theta_radian), -np.sin(theta_radian), 0., 0.],
+        [np.sin(theta_radian), np.cos(theta_radian), 0., 0.],
+        [0., 0., 1., 0.],
+        [0., 0., 0., 1.]
+    ])
+    return transform(m, rotation_matrix)
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
+    arr = np.random.rand(100, 3)
+
+    fig = plt.figure()
+    ax = Axes3D(fig)
+
+    ax.scatter(arr[:, 0], arr[:, 1], arr[:, 2])
+
+    # result = translate(arr, 2, 2, 2)
+    result = scale(arr, 3, 3, 3)
+    # result = rotateX(arr, 180)
+
+    ax.scatter(result[:, 0], result[:, 1], result[:, 2])
+
+    result2 = scale(arr, 2, 2, 2)
+    ax.scatter(result2[:, 0], result2[:, 1], result2[:, 2])
+
+    result3 = scale(arr, 2.5, 2.5, 2.5)
+    ax.scatter(result3[:, 0], result3[:, 1], result3[:, 2])
+
+    plt.show()
+
