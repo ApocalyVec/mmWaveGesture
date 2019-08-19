f56fd55d45963c6f58817a488d3b78df8a4d1250
wasd120d <490636518@qq.com>
2019-08-19 00:00:09 -0400

    finishing implementing thumouse regerssor with data gen

--- learn/thumouse_crnn_regressor_DataGen.py
+++ learn/thumouse_crnn_regressor_DataGen.py
@@ -13,31 +13,28 @@ import numpy as np
 import os
 import time
 
-from learn.classes import indexPenDataGen
+from learn.classes import indexPenDataGen, thumouseDataGen
 from utils import generate_train_val_ids
 
 if __name__ == '__main__':
-    dataGenParams = {'dim': (100, 1, 25, 25, 25),
+    dataGenParams = {'dim': (75, 1, 25, 25, 25),
                      'batch_size': 10,
-                     'n_classes': 5,
                      'shuffle': True}
 
-    label_dict_path = 'D:/indexPen/labels/label_dict.p'
-    partition = generate_train_val_ids(0.1)
+    dataset_path = 'D:/Programing/19Sum_ft_localization/mmWaveGesture/ThuMouse/dataset'
+    label_dict_path = 'D:/Programing/19Sum_ft_localization/mmWaveGesture/ThuMouse/labels/label_dict'
+    partition = generate_train_val_ids(0.1, dataset_path=dataset_path)
     labels = pickle.load(open(label_dict_path, 'rb'))
 
     ## Generators
-
     training_gen = thumouseDataGen(partition['train'], labels, **dataGenParams)
     validation_gen = thumouseDataGen(partition['validation'], labels, **dataGenParams)
 
-
     # Build the RNN ###############################################
-
     model = Sequential()
     model.add(
         TimeDistributed(Conv3D(filters=16, kernel_size=(3, 3, 3), data_format='channels_first', input_shape=(1, 25, 25, 25),
-                               activation='relu', kernel_regularizer=l2(0.0005)), input_shape=(100, 1, 25, 25, 25)))
+                               activation='relu', kernel_regularizer=l2(0.0005)), input_shape=(75, 1, 25, 25, 25)))
     model.add(TimeDistributed(BatchNormalization()))
     model.add(TimeDistributed(MaxPooling3D(pool_size=(2, 2, 2))))
 
@@ -55,22 +52,19 @@ if __name__ == '__main__':
     model.add(LSTM(units=64, return_sequences=False))
     model.add(Dropout(rate=0.5))
 
-    model.add(Dense(5, activation='softmax'))
+    model.add(Dense(2))
 
     epochs = 5000
 
-    adam = optimizers.adam(lr=1e-5, decay=1e-2 / epochs)
-    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+    # adam = optimizers.adam(lr=1e-5, decay=1e-2 / epochs)
+    model.compile(optimizer='adam', loss='mean_squared_error')
 
     # add early stopping
     es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=250)
     mc = ModelCheckpoint(
-        'D:/indexPen/trained_models/bestSoFar_indexPen_CRNN' + str(datetime.datetime.now()).replace(':', '-').replace(' ',
+        'D:/Programing/19Sum_ft_localization/mmWaveGesture/ThuMouse/trained_models/bestSoFar_thuMouse_CRNN' + str(datetime.datetime.now()).replace(':', '-').replace(' ',
                                                                                                                       '_') + '.h5',
-        monitor='val_acc', mode='max', verbose=1, save_best_only=True)
-
-    # history = model.fit(X_train, Y_train, validation_data=(X_test, Y_test), shuffle=True, epochs=epochs,
-    #                     batch_size=10, callbacks=[es, mc])
+        monitor='val_loss', mode='max', verbose=1, save_best_only=True)
 
     history = model.fit_generator(generator=training_gen, validation_data=validation_gen, use_multiprocessing=True, workers=6, shuffle=True, epochs=epochs, callbacks=[es, mc])
 
