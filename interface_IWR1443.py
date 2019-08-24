63477e7921c842f141182a4b7f850d083cab32c3
wasd120d <490636518@qq.com>
2019-08-23 22:24:35 -0400

    add frameDrop rate calculation

--- interface_IWR1443.py
+++ interface_IWR1443.py
@@ -143,7 +143,7 @@ while True:
             # frameRow = np.asarray([detObj['x'], detObj['y'], detObj['z'], detObj['doppler']]).transpose()
             # preprocessed_frameArray.append(preprocess_frame(frameRow))
 
-            # time.sleep(0.033)  # This is framing frequency Sampling frequency of 30 Hz
+            time.sleep(0.033)  # This is framing frequency Sampling frequency of 30 Hz
 
         if interrupt_list:
             raise KeyboardInterrupt()
@@ -159,7 +159,16 @@ while True:
         main_stop_event.set()
 
         # save radar frame data
+        print('Sensor Stopped')
 
+        if len(frameData) != 0:
+            print('The number of frame collected is ' + str(len(frameData)))
+            time_record = max(x[0] for x in frameData) - min(x[0] for x in frameData)
+            expected_frame_num = time_record * 20
+            frame_drop_rate = len(frameData) / expected_frame_num
+            print('Recording time is ' + str(time_record))
+            print('The expected frame num is ' + str(expected_frame_num))
+            print('Frame drop rate is ' + str(1 - frame_drop_rate))
         is_save = input('do you wish to save the recorded frames? [y/n]')
 
         if is_save == 'y':
